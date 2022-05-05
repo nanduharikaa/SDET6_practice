4d0149602c2244e4428ee5ddc5f4bb7053e6c06f
Sindh <Sindh@192.168.0.4>
2022-05-05 21:08:21 +0530

    create_org create_deleting

---
+++ src/Org/Create_Org.java
@@ -0,0 +1,8 @@
+package Org;
+
+public class Create_Org {
+	public static void main(String[]args) {
+		System.out.println("create org");
+	}
+
+}
---
+++ src/Org/Deleting.java
@@ -0,0 +1,8 @@
+package Org;
+
+public class Deleting {
+	public static void main(String[]args) {
+		System.out.println("deleting");
+	}
+
+}
